56d038ed2861569576107918a75d5d2945a6e279
malvarez <manuel.alvarez.alvarez@gmail.com>
2018-12-01 18:50:15 +0100

    LOGGER

--- src/log.js
+++ src/log.js
@@ -1,88 +1,173 @@
-class Level {
-  constructor(value, adapter) {
-    this.value = value;
-    this.adapter = adapter;
+export class Level {
+  constructor(name, value) {
+    this._name = name;
+    this._value = value;
   }
 
   isLoggable(level) {
-    return this.value <= level;
+    return this.value >= level.value;
   }
 
-  log(pattern, ...args) {
-    const message =
-      args.length > 0 ? Level._replaceAll(pattern, args) : pattern;
-    this.adapter().call(message);
+  get value() {
+    return this._value;
   }
 
-  static _replaceAll(pattern, ...args) {
-    let message = "";
-    const expr = /\{(\d*)\}/g;
-    let match,
-      patternIndex = 0,
-      argIndex = 0;
-    while ((match = expr.exec(pattern))) {
-      message += pattern.substring(patternIndex, match.index);
-      patternIndex = match.index + match[0].length;
-      message += match[1].length > 0 ? args[match[1]] : args[argIndex++];
+  get name() {
+    return this._name;
+  }
+}
+
+Level.ALL = new Level("ALL", Number.MAX_SAFE_INTEGER);
+Level.DEBUG = new Level("DEBUG", 100);
+Level.INFO = new Level("INFO", 0);
+Level.ERROR = new Level("ERROR", -100);
+Level.OFF = new Level("OFF", Number.MIN_SAFE_INTEGER);
+
+export class Logger {
+  constructor(parent, name, level, pattern, appender) {
+    this._parent = parent;
+    this._name = name;
+    this._level = level;
+    this._pattern = pattern;
+    this._appender = appender;
+  }
+
+  log(level, message, ...args) {
+    if (this.level.isLoggable(level)) {
+      const messageWithArgs =
+        args.length === 0
+          ? message
+          : this._injectArguments.apply(
+              this,
+              Array.prototype.splice.call(arguments, 1)
+            );
+      const finalMessage = this._composeMessage(messageWithArgs);
+      this.appender(finalMessage);
     }
-    if (patternIndex < pattern.length) {
-      message += pattern.substring(patternIndex);
+  }
+
+  info(message, ...args) {
+    const newArguments = Array.prototype.slice.call(arguments);
+    newArguments.unshift(Level.INFO);
+    this.log.apply(this, newArguments);
+  }
+
+  debug(message, ...args) {
+    const newArguments = Array.prototype.slice.call(arguments);
+    newArguments.unshift(Level.DEBUG);
+    this.log.apply(this, newArguments);
+  }
+
+  error(message, ...args) {
+    const newArguments = Array.prototype.slice.call(arguments);
+    newArguments.unshift(Level.ERROR);
+    this.log.apply(this, newArguments);
+  }
+
+  _composeMessage(message) {
+    return this._withRegexp(/%(-?\d*)?(\w+)/g, this.pattern, match => {
+      let result;
+      switch (match[2]) {
+        case "level":
+          result = this.level.name;
+          break;
+        case "date":
+          result = new Date().toISOString();
+          break;
+        case "name":
+          result = this.name;
+          break;
+        case "message":
+          result = message;
+          break;
+      }
+      return this._justify(match[1] ? parseInt(match[1]) : 0, result);
+    });
+  }
+
+  _justify(value, message) {
+    if (value === 0) {
+      return message;
     }
     return message;
   }
-}
 
-export function log(level, pattern, ...args) {
-  if (level.isLoggable(LEVEL)) {
-    level.log(pattern, ...args);
+  _injectArguments(message, ...args) {
+    let argIndex = 0;
+    return this._withRegexp(
+      /\{(\d*)\}/g,
+      message,
+      match => args[match[1].length > 0 ? parseInt(match[1]) : argIndex++]
+    );
+  }
+
+  _withRegexp(regexp, message, callback) {
+    let result = "";
+    let match,
+      start = 0;
+    while ((match = regexp.exec(message))) {
+      result += message.substring(start, match.index);
+      start = match.index + match[0].length;
+      result += callback(match);
+    }
+    if (start < message.length) {
+      result += message.substring(start);
+    }
+    return result;
+  }
+
+  get parent() {
+    return this._parent;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this._level || this.parent.level;
+  }
+
+  set level(level) {
+    this._level = level;
+  }
+
+  get appender() {
+    return this._appender || this.parent.appender;
+  }
+
+  set appender(appender) {
+    this._appender = appender;
+  }
+
+  get pattern() {
+    return this._pattern || this.parent.pattern;
+  }
+
+  set pattern(pattern) {
+    this._pattern = pattern;
   }
 }
 
-let OUT = console.log;
-let ERR = console.error || console.log;
-let NULL = () => {};
+export class LoggerFactory {}
 
-export const ALL = new Level(Number.MAX_SAFE_INTEGER, getOut);
-export const DEBUG = new Level(100, getOut);
-export const INFO = new Level(0, getOut);
-export const ERROR = new Level(-100, getErr);
-export const OFF = new Level(Number.MIN_SAFE_INTEGER, getNull);
-
-let LEVEL =
-  process &&
-  process.env &&
-  ["test", "development"].indexOf(process.env.NODE_ENV) >= 0
-    ? ALL.value
-    : OFF.value;
-
-export function setLevel(level) {
-  LEVEL = level.value;
-}
-
-export function getLevel() {
-  return LEVEL;
-}
-
-export function setOut(stream) {
-  OUT = stream;
-}
-
-export function setErr(stream) {
-  ERR = stream;
-}
-
-export function setNull(stream) {
-  NULL = stream;
-}
-
-export function getOut() {
-  return OUT;
-}
-
-export function getErr() {
-  return ERR;
-}
-
-export function getNull() {
-  return NULL;
-}
+LoggerFactory._loggers = {
+  "": new Logger(
+    null,
+    "root",
+    Level.ERROR,
+    "%5level (%date) [%-10name]: %message",
+    console.log
+  )
+};
+LoggerFactory.getRootLogger = function() {
+  return LoggerFactory._loggers[""];
+};
+LoggerFactory.getLogger = function(name) {
+  let logger = LoggerFactory._loggers[name];
+  if (!logger) {
+    logger = new Logger(LoggerFactory._loggers[""], name);
+    LoggerFactory._loggers[name] = logger;
+  }
+  return logger;
+};
--- test/log.test.js
+++ test/log.test.js
@@ -1,44 +1,99 @@
-import {
-  log,
-  setOut,
-  getOut,
-  setErr,
-  getErr,
-  setLevel,
-  getLevel,
-  ALL,
-  INFO,
-  DEBUG,
-  ERROR,
-  OFF
-} from "../src/log.js";
+import { LoggerFactory, ALL, Level } from "../src/log.js";
 
 describe("The log facility works", () => {
-  const logged = [];
-  const out = getOut();
+  const logger = LoggerFactory.getRootLogger();
+  const defaultAppender = logger.appender;
+
+  const messages = [];
 
   beforeEach(() => {
-    setOut(m => logged.push(m));
+    logger.appender = message => messages.push(message);
   });
 
   afterEach(() => {
-    logged.length = 0;
-    setOut(out);
+    messages.length = 0;
+    logger.appender = defaultAppender;
   });
 
-  test("The default level in the tests is ALL", () => {
-    expect(getLevel()).toEqual(ALL.value);
+  test("The default level in the tests is ERROR", () => {
+    expect(logger.level).toEqual(Level.ERROR);
+
+    logger.log(Level.INFO, "Hello!");
+    expect(messages.length).toEqual(0);
   });
 
-  test("The library will log statements to the console", () => {
-    log(INFO, "Hello!");
-    expect(logged.length).toEqual(1);
-    expect(logged[0]).toEqual("Hello!");
+  test("The logger will log statements to the chosen appender", () => {
+    logger.log(Level.ERROR, "Hello!");
+    expect(messages.length).toEqual(1);
+    expect(messages[0]).toContain("Hello!");
   });
 
-  test("The library can use {} as place holders", () => {
-    log(INFO, "Hello {}!", "Manuel");
-    expect(logged.length).toEqual(1);
-    expect(logged[0]).toEqual("Hello!");
+  test("The logger can use {} as place holders", () => {
+    logger.log(Level.ERROR, "Hello {}!", "Manuel");
+    expect(messages.length).toEqual(1);
+    expect(messages[0]).toContain("Hello Manuel!");
+
+    logger.log(Level.ERROR, "Hello {}", "Manuel");
+    expect(messages.length).toEqual(2);
+    expect(messages[1]).toContain("Hello Manuel");
+
+    logger.log(Level.ERROR, "{} Manuel!", "Hello");
+    expect(messages.length).toEqual(3);
+    expect(messages[2]).toContain("Hello Manuel!");
+  });
+
+  test("The logger will use the polymorphic methods just fine", () => {
+    logger.error("Hello {}!", "Manuel");
+    logger.info("Hello {}!", "Manuel");
+    logger.debug("Hello {}!", "Manuel");
+
+    expect(messages.length).toEqual(1);
+    expect(messages[0]).toContain("Hello Manuel!");
+  });
+
+  test("The logger can use {} with indexes as place holders", () => {
+    logger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(messages.length).toEqual(1);
+    expect(messages[0]).toContain("Hello Mr. Manuel!");
+  });
+
+  test("The logger factory can create new loggers", () => {
+    const customLogger = LoggerFactory.getLogger("custom");
+    expect(customLogger.name).toEqual("custom");
+
+    customLogger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(messages.length).toEqual(1);
+  });
+
+  test("The new loggers can change its own level", () => {
+    const customLogger = LoggerFactory.getLogger("custom");
+    customLogger.level = Level.OFF;
+    customLogger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(messages.length).toEqual(0);
+
+    logger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(messages.length).toEqual(1);
+  });
+
+  test("The new loggers can change its own pattern", () => {
+    const customLogger = LoggerFactory.getLogger("custom");
+    customLogger.pattern = "%message";
+    customLogger.level = Level.ERROR;
+    customLogger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(messages.length).toEqual(1);
+    expect(messages[0]).toEqual("Hello Mr. Manuel!");
+  });
+
+  test("The new loggers can change its own appender", () => {
+    const customLogger = LoggerFactory.getLogger("custom");
+    const otherMessages = [];
+    customLogger.level = ALL;
+    customLogger.appender = message => otherMessages.push(message);
+    customLogger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(otherMessages.length).toEqual(1);
+    otherMessages.length = 0;
+
+    logger.log(Level.ERROR, "Hello {1} {0}!", "Manuel", "Mr.");
+    expect(messages.length).toEqual(1);
   });
 });
